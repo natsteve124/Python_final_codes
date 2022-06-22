b9fb83776ee4195f58b9089f33eb00888112c554
natsteve124 <107065263+natsteve124@users.noreply.github.com>
2022-06-22 09:52:52 -0700

    Create scratch.py

---
+++ scratch.py
@@ -0,0 +1,29 @@
+# This will be my final project for class
+# It's a simple password decoder / breaker
+
+# import random
+# number = int(input("Input a number that is a password: "))
+# guess = 0
+# while (guess != number):
+ # guess = random.randint(0,9999)
+# print(guess)
+# print("Your password is " + str(number))
+
+# This code will run until it finds your password !!!
+
+# This next simple password decoder / breaker has both letters and numbers
+
+import random
+character = "0123456789abcdefghijklmnopqrstuvwxyz"
+character_list = list(character)
+password = input("Enter your password: ")
+guess = ""
+while (guess != password):
+     guess = random.choices(character_list,k=len(password))
+     print(guess)
+     guess = "".join(guess)
+     print("your password is " + guess)
+
+# I hope you enjoy this Justin try it out :)
+
+# this code was sited from Johnathan Karr on youtube https://www.youtube.com/watch?v=P5Lt8J3_ZnI
